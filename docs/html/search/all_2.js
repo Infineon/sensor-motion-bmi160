ee233243e96e7f322aa4ef37c795f3b471c5eb30
gitlab-runner <do-not-reply@cypress.com>
2020-06-23 18:16:01 -0500

    Upload sensor-motion-bmi160 1.0.0.15346

---
+++ docs/html/search/all_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['cyhal_5fbmi160_5frslt_5ferr_5fadditional_5fint_5fpin',['CYHAL_BMI160_RSLT_ERR_ADDITIONAL_INT_PIN',['../group__group__board__libs.html#ga6a782574f531667dbb853ad8e068cda8',1,'mtb_bmi160.h']]]
+];
